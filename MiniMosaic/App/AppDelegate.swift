7e739c8f675050bc999437b5b237124f83979ab8
freegatik <anton.soloviev2016@gmail.com>
2023-01-12 13:11:26 +0300

    Bootstrap UIApplication entry point and UIScene-driven window setup
    Add AppDelegate with scene session hook and SceneDelegate that owns the key window.

---
+++ MiniMosaic/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  MiniMosaic
+//
+//  Created by Богдан Тарченко on 03.09.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: UISceneSession Lifecycle
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
---
+++ MiniMosaic/App/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  MiniMosaic
+//
+//  Created by Богдан Тарченко on 03.09.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = MiniAppListViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
